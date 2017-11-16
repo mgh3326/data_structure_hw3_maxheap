464b06f8524471a7d21a54394e8ebb8c79f10282
mgh3326 <mgh3326@naver.com>
2017-11-16 11:45:22 +0900

    not_rotated_form()

--- data_structure_hw3_maxheap/Source.cpp
+++ data_structure_hw3_maxheap/Source.cpp
@@ -14,7 +14,7 @@ public:
 	HeapNode(int k = 0) :key(k) {}
 	void setKey(int k) { key = k; }
 	int getKey() { return key; }
-	void display() { printf("%4d", key); }
+	void display() { printf("%d", key); }
 };
 class MaxHeap {
 	HeapNode node[MAX_ELEMENT];//요소의 배열
@@ -73,6 +73,76 @@ public:
 		}
 		printf("\n---------------------------------");
 	}
+	void rotated_form() {
+		
+		if (size < 3)
+		{
+			for (int i = 1, level = 2; i <= size; i++) {
+				if (i == level) {
+					printf("\n    ");
+					level *= 2;
+				}
+				node[i].display();
+			}
+			printf("\n---------------------------------");
+		}
+		else if (size < 7)
+		{
+			for (int i = 1, level = 2; i <= size; i++) {
+				if (i == level) {
+					printf("\n    ");
+					level *= 2;
+				}
+				node[i].display();
+			}
+			printf("\n---------------------------------");
+		}
+	}
+	void not_rotated_form() {
+			for (int i = 1, level = 1; i <= size; i++) {
+				if (i == level) {
+					
+					printf("\n");
+					for (int j = 0; j < 127/level; j++)
+					{
+						cout << "a";
+					}
+					level *= 2;
+				}
+
+				node[i].display();
+			}
+		
+		
+		printf("\n---------------------------------");
+	}
+	void H_tree_form() {
+	/*	str[] = "123456789ABCDEFGHIJKLMNOPQRSTUV";
+		V[4][2] = { { -1, 0 },{ 1, 0 },{ 0, 1 },{ 0, -1 } };
+		H(node, i, j, d, U, D, R, L)
+		{
+			if (node > n) return;
+			H_tree[i][j] = str[node - 1];
+			if (2 * node <= n) {
+				H_tree[i + d*V[L][0]][j + d*V[L][1]] = str[2 * node - 1];
+				H(4 * node, i + d*(V[L][0] + V[U][0]),
+					j + d*(V[L][1] + V[U][1]), d / 2, D, U, L, R);
+				H(4 * node + 1, i + d*(V[L][0] + V[D][0]),
+					j + d*(V[L][1] + V[D][1]), d / 2, U, D, R, L);
+			}
+			if (2 * node + 1 <= n) {
+				H_tree[i + d*V[R][0]][j + d*V[R][1]] = str[2 * node];
+				H(4 * node + 2, i + d*(V[R][0] + V[D][0]),
+					j + d*(V[R][1] + V[D][1]), d / 2, U, D, R, L);
+				H(4 * node + 3, i + d*(V[R][0] + V[U][0]),
+					j + d*(V[R][1] + V[U][1]), d / 2, D, U, L, R);
+			}
+		}
+		center(n) { return n <= 1 ? 0 : 2 * center(n / 4) + 1; }
+		depth(n) { return n <= 7 ? 1 : 2 * depth(n / 4); }
+		CALL  H(1, center(n), center(n), depth(n), N, S, E, W);*/
+
+	}
 };
 //주함수
 void main() {
@@ -80,14 +150,15 @@ void main() {
 
 	//삽입 테스트
 	heap.insert(10);	heap.insert(5);
-	heap.insert(30);	heap.insert(8);
+	heap.insert(30);	
+	heap.insert(8);
 	heap.insert(9);	heap.insert(3);
 	heap.insert(7);
-	heap.display();//상태 출력
+	heap.not_rotated_form();//상태 출력
 
 				   //삭제 테스트
-	heap.remove();	heap.display();
-	heap.remove();	heap.display();
+	/*heap.remove();	heap.display();
+	heap.remove();	heap.display();*/
 	printf("\n");
 	// 파일 읽기 준비
 	/*ifstream in("input.txt");*/
@@ -116,11 +187,33 @@ void main() {
 	in.close();*/
 	char ch;
 	fstream fin("input.txt", fstream::in);
+	fin >> noskipws >> num;
 	while (fin >> noskipws >> ch) {
 		if(ch!=' '&&ch!='\n')
 		v.push_back(ch); // Or whatever
 	}
+	//num = v[0];
 	for (int i = 0; i < v.size(); i++)
 		cout << v[i] << endl;
-	num = v[0];
+	//int n1 = 63;
+	//int n2 = 2;
+	//cout << n1 / n2 << endl;
 }
+
+//1	                                   	                                   	                                                        0
+//2	                                   	                               0                                                                                                                               1
+//4	                                   0                                                               1                                                               2                                                               3
+//8	                   0                               1                               2                               3                               4                               5                               6                               7
+//16	       0               1               2               3               4               5               6               7               8               9               0               1               2               3               4               5
+//32	   0       1       2       3       4       5       6       7       8       9       0       1       2       3       4       5       6       7       8       9       0       1       2       3       4       5       6       7       8       9       0       1
+//64	 0   1   2   3   4   5   6   7   8   9   0   1   2   3   4   5   6   7   8   9   1   2   3   4   5   6   7   8   9   1   2   3   4   5   6   7   8   9   1   2   3   4   5   6   7   8   9   1   2   3   4   5   6   7   8   9   0   1   2   3   4   5   6   7
+//128	0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+//
+//128은 한칸씩
+//64는 처음에 한칸 그 다음 3칸씩
+//32는 처음에 세칸 그 다음 7칸씩
+//16은 처음에 7칸 그 다음 15칸
+//8은 처음에 15칸 그다음 31칸
+//4는 31칸,63칸
+//2는 63칸,127칸
+//1은 127칸
